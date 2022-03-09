4a1a66f378b9402bd187f62e75d587084c95caf1
blq200 <92025169+blq200@users.noreply.github.com>
2022-03-09 12:55:00 +0100

    Add files via upload

---
+++ sitevitrine/footer.php
@@ -0,0 +1,51 @@
+
+
+<footer class=" text-center text-white" style="background-color:#D6B89C; margin-top:1em;">
+          <!-- Grid container -->
+          <div class="container p-4 pb-0">
+          <section class="mb-4">
+               <img style="width:7em;" src="logo2.png">
+               <p style=" color:white; font-family:Sarala;">Here you will find everything belongs to you My Queen</p>
+          
+          </section>
+            <!-- Section: Social media -->
+            <section class="mb-4">
+              <!-- Facebook -->
+              <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998;" href="https://www.facebook.com/SokoGlam/" role="button"><i
+                  class="fab fa-facebook-f"></i></a>
+        
+              <!-- Twitter -->
+              <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee;" href="https://twitter.com/sokoglam?ref_src=twsrc%5Egoogle%7Ctwcamp%" role="button"><i
+                  class="fab fa-twitter"></i></a>
+        
+              <!-- Google -->
+              <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39;" href="https://sokoglam.com/collections/newly-curated" role="button"><i
+                  class="fab fa-google"></i></a>
+        
+              <!-- Instagram -->
+              <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac;" href="https://www.instagram.com/sokoglam/?hl=fr" role="button"><i
+                  class="fab fa-instagram"></i></a>
+        
+              <!-- Linkedin -->
+              <a class="btn btn-primary btn-floating m-1" style="background-color: #0082ca;" href="https://www.linkedin.com/company/soko-glam" role="button"><i
+                  class="fab fa-linkedin-in"></i></a>
+            </section>
+            <!-- Section: Social media -->
+          </div>
+          <!-- Grid container -->
+        
+          <!-- Copyright -->
+          <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2022 Copyright:
+            <img src="logo2.png" style="width:6em;" >
+          </div>
+          <!-- Copyright -->
+        </footer>
+        
+
+  <!-- MDB -->
+<!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.js"
+></script>
---
+++ sitevitrine/vitrine.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!------------------------Link Bootstrap------------------------>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+    <!------------------------Font Family--------------------------->
+    <link href="https://fonts.googleapis.com/css2?family=Coda&family=Quintessential&family=Red+Rose:wght@600&family=Risque&family=Roboto+Mono:wght@100&family=Roboto+Slab:wght@600&family=Roboto:ital,wght@0,700;0,900;1,500;1,700;1,900&family=Rubik:wght@600&family=Sawarabi+Mincho&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Coda&family=Quintessential&family=Red+Rose:wght@600&family=Risque&family=Roboto+Mono:wght@100&family=Roboto+Slab:wght@600&family=Roboto:ital,wght@0,700;0,900;1,500;1,700;1,900&family=Rubik:wght@600&family=Sarpanch:wght@700&family=Sawarabi+Mincho&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Coda&family=Quintessential&family=Red+Rose:wght@600&family=Risque&family=Roboto+Mono:wght@100&family=Roboto+Slab:wght@600&family=Roboto:ital,wght@0,700;0,900;1,500;1,700;1,900&family=Rubik:wght@600&family=Sarala&family=Sarpanch:wght@700&family=Sawarabi+Mincho&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Coda&family=Quintessential&family=Red+Rose:wght@600&family=Risque&family=Roboto+Mono:wght@100&family=Roboto+Slab:wght@600&family=Roboto:ital,wght@0,700;0,900;1,500;1,700;1,900&family=Rubik:wght@600&family=Sarala&family=Sarina&family=Sarpanch:wght@700&family=Sawarabi+Mincho&display=swap" rel="stylesheet">
+    <!-------------StyleSheet----------->
+    <link rel="stylesheet" href="style.css">
+    <!-- Font Awesome -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+    
+    <!---------------------------Font Awsame------------------->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <title>Site Vitrine</title>
+</head>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css"
+  rel="stylesheet"
+/>
+<body style="background-color:#F1E6DC;">
+  <?php
+    include'navbar.php';
+    include'main.php';
+    include'footer.php';
+  ?>
+
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <body style="background-color:#F1E6DC;"> -->
+  <!--------------------------Header------------------------->
+<!-- <header>
+<nav class="navbar navbar-expand-lg navbar-light "  style = "background:#D6B89C; height:4em;">
+  <div class="container-fluid">
+    <img src="logo1.png" style="width:8em;">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item" >
+          <a style="color:white; font-family:Sawarabi Mincho;margin-left:70em;"class="nav-link active" aria-current="page" href="#">Products</a>
+        </li>
+        <li class="nav-item">
+          <a style="color:white;font-family:Sawarabi Mincho;" class="nav-link" href="#">About Us</a>
+        </li>
+        <li class="nav-item">
+        <a href=""><i style="color:white;" class="bi bi-person"></i></a>
+        </li>
+        <li class="nav-item">
+        <a href=""><i class="bi bi-heart" style="color:white;"></i></a>
+        </li>
+        <li class="nav-item">
+        <a href=""><i style="color:white;" class="bi bi-cart"></i></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  <img src="header2.png" style="width:100%; height:15em;" >
+</header> -->
+  <!------------------------------Main-------------------------->
+    <!-- <main>
+    <div class="card" style="width: 18rem; margin-top:2em; margin-left:1em; background-color:#D3A589;">
+  <img src="img1.png" class="card-img-top">
+  <div class="card-body">
+    <h5 class="card-title" style="text-align:center;">Pro Brush Set</h5>
+    <p class="card-text" style="text-align:center;">Price:99$</p>
+    <a href="#"><i class="bi bi-heart" style="color:white;"></i></a>
+    <a href="#" class="btn " style="background-color:#A36D79; width:7em; margin-left:4em;">BY NOW</a>
+  </div>
+</div>
+    </main> -->
+  <!---------------------------Footer------------------------->
+    <!-- <footer style="background-color:#D6B89C; padding: 7em 0;height:15em; margin-top:3.5em;">
+
+    <ul style="display:block;">
+       <img src="logo2.png" style="margin-top:-7em; margin-left:1em; width:10em;">
+       <p style="margin-left:2em; margin-top:-2em; color:white; font-family:Sarala;">Here you will find <br> everything belongs to you <br> My Queen</p> -->
+      <!--List1-->
+       <!-- <li style="list-style-type:none; margin-left:20em; margin-top:-8em; ">
+       <h4 style="color:#8B6147; font-family:sarala; ">Locate Us</h4>
+       <p style="color:white; font-family:sarala;">Bni Makada,Tanger </p>
+       </li> -->
+      <!--List2-->
+       <!-- <li style="list-style-type:none; margin-left:20em; ">
+        <h4 style="color:#8B6147; font-family:sarala;">Phone</h4>
+        <p style="color:white; font-family:sarala;">0571277355</p>
+       </li> -->
+      <!--List3-->
+       <!-- <li style="list-style-type:none; margin-left:39em; margin-top:-9em;">
+       <h4 style="color:#8B6147; font-family:sarala;">E-mail</h4>
+        <p style="color:white; font-family:sarala;">maketaj@gmail.com</p>
+       </li> -->
+      <!--List4-->
+       <!-- <li style= "list-style-type:none; margin-left:39em;">
+          <h4 style="color:#8B6147; font-family:sarala;">Contact Us</h4>
+          <a href="https://www.facebook.com/SokoGlam/"><i style="color:white;" class="bi bi-facebook"></i></a> 
+          <a href="https://www.instagram.com/sokoglam/?hl=fr"> <i style="color:white; margin-left:1em;" class="bi bi-instagram"></i></a>
+          <a href="https://twitter.com/sokoglam?ref_src=twsrc%5Egoogle%"><i style="color:white; margin-left:1em;" class="bi bi-twitter"></i></a>
+       </li>
+
+      </ul>
+      <p style="background-color:#BB8758; text-align:center; font-family:sarala;">© CopyRight.2022 March by <p style="color:#8B6147; margin-left:32.3em; margin-top:-2.5em; font-family:Sarina;">MakeTAJ</p></p>
+    </footer> -->
